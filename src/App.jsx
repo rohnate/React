58da4ed740255fc4e927072591e2197e3388cdf7
itsRss <164704500+rohnate@users.noreply.github.com>
2026-02-11 11:55:30 +0000

    react useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+// defining the component
+function Counter() {
+  const [count, setCounter] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((count) => count + 1);
+    }, 1000);
+  }, []); // dependancy array
+
+  function increaseCount() {
+    setCounter(count + 1);
+  }
+  function decreaseCount() {
+    setCounter(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increaseCount}>increase Counter</button>
+      <button onClick={decreaseCount}>decrease Counter</button>
+    </div>
+  );
+}
+
+// main function where the component render
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <div>
+        <h1>Hi, there</h1>
+        <Counter></Counter>
+      </div>
     </>
   );
 }
