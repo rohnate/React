c6d9a46c120bd14f5a6701a23f411513e23d9d42
itsRss <164704500+rohnate@users.noreply.github.com>
2026-02-20 06:17:54 +0000

    setting up routing in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home";
 import Part1 from "./part1";
 
 function App() {
   return (
     <>
-      {/* <Part1 /> */}
-      <div>Heelo world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/learning/part1" element={<Part1 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import Part1 from "./part1";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Welcome to Home page.</h2>
+      <h4>Here we are Learning React from scratch.</h4>
+      <Link to="/learning/part1">Part 1 - page</Link>{" "}
+      {/* to expects a string path, not a component. */}
+    </>
+  );
+}
